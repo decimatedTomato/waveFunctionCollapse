46d172afee8f37128cdf590032138258c22166c4
decimatedTomato <87151993+decimatedTomato@users.noreply.github.com>
2022-07-13 11:39:22 +0200

    This commit aims to overhaul the implemented "backtracking". Now if no valid tiles are found the most recently placed tile is deleted and the program attempts to place another valid option in its place. If there is no valid option the program should attempt to collapse a different grid element instead. The case where a new grid element should be found because the minimum entropy level is empty has not been handled and will currently prevent execution.

--- generation.py
+++ generation.py
@@ -10,10 +10,9 @@ from tiles import tiles
 @dataclass(frozen=True)
 class Choice:
     """Class that is used to record all of the random decisions the program makes"""
-    type: bool  # 1 for tile, 0 for grid element
-    choices: list[tuple[int, int]]  # in order that will be attempted
-    attempts: int
-
+    tile_options: Optional[list[int]]
+    grid_options: Optional[list[tuple[int, int]]]
+    # options are listed in order that will be attempted
 
 @dataclass()
 class GridElement:
@@ -34,24 +33,26 @@ class TileGrid:
         # Grid
         self.size: tuple[int, int] = (self.rows, self.columns)
         self.grid_array: np.ndarray[GridElement] = np.ndarray(shape=self.size, dtype=object)
+        """Using numpy for the array may have been a mistake. It doesn't add much except for problems.
+        Since I don't really understand dtypes that aren't numeric the ide doesn't suggest grid element attributes."""
         for x in range(self.columns):
             for y in range(self.rows):
                 self.grid_array[x, y] = GridElement(None)
-        # For every random choice the list of choices and the attempted choices should be saved.
-        # This feels like it's going to run into memory issues very quickly.
-        # But how else should I do it?
-        self.history: list[tuple[bool, tuple[int, int]]] = []
-        """
-        Using numpy for the array may have been a mistake. It doesn't add much except for problems.
-        Since I don't really understand dtypes that aren't numeric the ide doesn't suggest grid element attributes.
-        """
 
-    def entropy_of(self, x, y) -> int:
+        """ For every random choice the list of choices and the attempted choices should be saved.
+        This feels like it's going to run into memory issues very quickly.
+        But how else should I do it? """
+        self.last_pos: tuple[int, int]
+        self.history: list[Choice] = []  # field(default_factory=list)
+        self.decisions: int = 0
+
+    def entropy_of(self, pos: tuple[int, int]) -> int:
         """Returns the number of tiles that have valid borders with the tile of the 4 bordering grid elements"""
-        return len(self.valid_neighbors(x, y))
+        return len(self.valid_neighbors(pos))
 
-    def valid_neighbors(self, x, y):
+    def valid_neighbors(self, pos: tuple[int, int]):
         # First add the existing adjacent edges into a list (using None if the adjacent tile is empty)
+        x, y = pos
         adjacent_edges = []
         if y - 1 < 0 or self.grid_array[x, y - 1].tile_id is None:
             adjacent_edges.append(None)
@@ -87,55 +88,86 @@ class TileGrid:
 
         return valid_tiles
 
+    def recollapse_tile(self, chosen_id: int, chosen_pos: tuple[int, int]):
+        element: GridElement = self.grid_array[chosen_pos]
+        element.tile_id = chosen_id
+        self.propogate_change(chosen_pos)
+        self.last_pos = chosen_pos
+        self.decisions += 1
+
+    def backtrack(self):
+        """This function removes the most recently changed tile and resets the entropy of its neighbors"""
+        pos = self.last_pos
+        print("pos:", pos)
+        print("tile options", self.history[-1].tile_options)
+        element = self.grid_array[pos]
+        element.tile_id = None
+        element.entropy = self.entropy_of(pos)
+        self.propogate_change(pos)
+        self.decisions -= 1
+
     def collapse_tile(self, element_pos: tuple[int, int]):
         """When this function is called on a specific tile a valid tile id is selected and it becomes collapsed"""
         element: GridElement = self.grid_array[element_pos]
-        pot_tiles = self.valid_neighbors(element_pos[0], element_pos[1])
+        pot_tiles = self.valid_neighbors(element_pos)
+        options = [tiles.index(pot_tile) for pot_tile in pot_tiles]
+        random.shuffle(options)
+        if len(options) == 1:
+            print(f"for tile: {element_pos} there is only 1 option")
+        else:
+            print("Multiple options", options)
+        element.tile_id = options.pop()
+        self.propogate_change(element_pos)
+        self.history.append(Choice(tile_options=options, grid_options=[element_pos]))
+        self.last_pos = element_pos
+        self.decisions += 1
 
-        r = random.randint(0, len(pot_tiles) - 1)
-        # r corresponds to the randomly selected tile out of the short list pot_tiles
-        # now I need to find the index of that tile within the tiles list
-        chosen_tile_id = tiles.index(pot_tiles[r])
-        element.tile_id = chosen_tile_id
-        self.propogate_change(element_pos[0], element_pos[1])
-        # TODO self.history.append()
-        pass
-
-    def propogate_change(self, x: int, y: int):
+    def propogate_change(self, pos: tuple[int, int]):
+        x, y = pos
         if not y - 1 < 0:
-            self.grid_array[x, y - 1].entropy = self.entropy_of(x, y - 1)
-
+            self.grid_array[x, y - 1].entropy = self.entropy_of((x, y - 1))
         if not x + 1 > self.columns - 1:
-            self.grid_array[x + 1, y].entropy = self.entropy_of(x + 1, y)
-
+            self.grid_array[x + 1, y].entropy = self.entropy_of((x + 1, y))
         if not y + 1 > self.rows - 1:
-            self.grid_array[x, y + 1].entropy = self.entropy_of(x, y + 1)
-
+            self.grid_array[x, y + 1].entropy = self.entropy_of((x, y + 1))
         if not x - 1 < 0:
-            self.grid_array[x - 1, y].entropy = self.entropy_of(x - 1, y)
-
-    def backtrack(self):
-        action = self.history.pop()
-        past_pos = action[1]
-        self.grid_array[past_pos].tile_id = None
-        self.recalculate_entropy()
-        # self.grid_array[past_pos].entropy = self.entropy_of(past_pos[0], past_pos[1])
+            self.grid_array[x - 1, y].entropy = self.entropy_of((x - 1, y))
 
     def iterate(self) -> bool:
         """Chooses a tile with the lowest entropy, collapses that tile"""
+
+        # If this decision point has been reached through backtracking, instead utilize the history
+        if self.decisions < len(self.history):
+            choice = self.history.pop()
+            if not choice.grid_options:
+                print("No tile nor grid options left")
+                return False
+            if choice.tile_options:
+                # I need to handle the case where it was later decided that the most recent tile placement should
+                # be reconsidered
+                self.recollapse_tile(choice.tile_options.pop(), choice.grid_options[0])
+                self.history.append(Choice(choice.tile_options, choice.grid_options))
+                return True
+            print("All tile options of selected grid element coordinates have been exhausted. Good Luck!")
+            self.collapse_tile(choice.grid_options.pop())
+            self.history.append(Choice(grid_options=choice.grid_options, tile_options=None))
+            self.decisions += 1
+            return True
+
+        # General case where decision is being evaluated for the first time
         min_entropy = None
         for i in range(self.columns):
             for j in range(self.rows):
                 element: GridElement = self.grid_array[i, j]
+
                 # Skip collapsed elements
                 if element.tile_id is not None:
                     continue
 
                 # Undo unsolvable tiles
                 if element.entropy == 0:
-                    # TODO self.backtrack()
-                    # return True
-                    return False
+                    self.backtrack()
+                    return True
 
                 if min_entropy is None:
                     min_entropy = element.entropy
@@ -145,15 +177,11 @@ class TileGrid:
         if min_entropy is None:
             print("Generation completed")
             return False
-            # for x in range(self.columns):
-            #     for y in range(self.rows):
-            #         self.grid_array[x, y] = GridElement(None)
-            # self.recalculate_entropy()
-            # return True
         if min_entropy == 0:
-            print("Impossible position found")
+            print("Unreachable code reached")
+            return False
 
-        # Now create a list of the lowest entropy grid elements
+        # Create a list of the lowest entropy grid element coordinates
         pot_elements = []
         for i in range(self.columns):
             for j in range(self.rows):
@@ -162,31 +190,11 @@ class TileGrid:
                     continue
                 if element.entropy == min_entropy:
                     pot_elements.append((i, j))
-
-        # From the selected elements randomly choose one
-        # TODO history.append()
-        r = random.randint(0, len(pot_elements) - 1)
-        # Into collapse_tile I pass the position of the tile to be collapsed
-        self.collapse_tile(pot_elements[r])
-        # self.recalculate_entropy() should be unnecessary due to propogate()
-        # self.print_state()
+        random.shuffle(pot_elements)
+        self.collapse_tile(pot_elements.pop())
+        self.history.append(Choice(grid_options=pot_elements, tile_options=None))
+        self.decisions += 1
         return True
-
-    def recalculate_entropy(self):
-        """No longer necessary, inefficient solution to track the entropy of all tiles"""
-        for i in range(self.columns):
-            for j in range(self.rows):
-                self.grid_array[i, j].entropy = self.entropy_of(i, j)
-
-    def test(self):
-        self.grid_array[self.columns // 2, self.rows // 2].tile_id = 5
-        print("Central:", self.entropy_of(self.columns // 2, self.rows // 2))
-        print("North:", self.entropy_of(self.columns // 2, self.rows // 2 - 1))
-        print("East:", self.entropy_of(self.columns // 2 + 1, self.rows // 2))
-        print("South:", self.entropy_of(self.columns // 2, self.rows // 2 + 1))
-        print("West:", self.entropy_of(self.columns // 2 - 1, self.rows // 2))
-
-        self.collapse_tile((self.columns // 2, self.rows // 2))
 
     def print_state(self):
         # Print out the tile_id and entropy of every tile,
@@ -209,6 +217,12 @@ class TileGrid:
                 x_pos = x * self.pixels
                 y_pos = y * self.pixels
                 surface.blit(images[element.tile_id], (x_pos, y_pos))
+
+    def recalculate_entropy(self):
+        """No longer necessary, inefficient solution to track the entropy of all tiles"""
+        for i in range(self.columns):
+            for j in range(self.rows):
+                self.grid_array[i, j].entropy = self.entropy_of((i, j))
 
     def reset(self):
         for x in range(self.columns):
